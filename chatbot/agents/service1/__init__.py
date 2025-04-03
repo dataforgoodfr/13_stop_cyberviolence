c06811915f7934514c6190c403cb35b85fe8aebe
Emma Effa <eleffa@yahoo.com>
2025-04-03 11:31:09 +0200

    Update __init__.py
    Modification pour correction erreur pre-commit.

--- chatbot/agents/service1/__init__.py
+++ chatbot/agents/service1/__init__.py
@@ -1,3 +1,7 @@
-from .prompts import *
-from .service1 import *
+#from .prompts import *
+#from .service1 import *
+#modification pour correction erreur pre-commit
+from .prompts import SYSTEM_PROMPT, USER_PROMPT
+from .service1 import create_agent1, create_workflow
+
 
