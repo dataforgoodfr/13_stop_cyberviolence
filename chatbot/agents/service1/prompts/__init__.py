02080034235e56c9f0e68a302697668ab6287d4f
Heiner Atze <heiner.atze@gmx.net>
2025-04-19 13:47:38 +0200

    reorganize prompts to account of different model providers

--- chatbot/agents/service1/prompts/__init__.py
+++ chatbot/agents/service1/prompts/__init__.py
@@ -1,2 +1,13 @@
-from .prompts import *
-#Modification pour correction du pre-commit
+import os
+
+# import prompts based on model provider
+
+model_provider = os.getenv("SERVICE1_PROVIDER")
+if not model_provider:
+    raise EnvironmentError("SERIVCE1_PROVIDER env var needs to be defined")
+
+if model_provider == 'gemini':
+    from .gemini import *
+    
+elif model_provider == 'azure':
+    from .azure import *
